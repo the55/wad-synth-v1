4187620af123109a0dcd8f792a1be92f41c9f4f2
dnknapp <dan@dnknapp.com>
2020-04-18 09:16:13 -0400

    Cleaned up Key.js, removed visibility stuff

--- src/components/keyboard/Key.js
+++ src/components/keyboard/Key.js
@@ -1,28 +1,10 @@
 import React, { useContext, useEffect, createRef, useState } from 'react';
-import VisibilitySensor from 'react-visibility-sensor';
 import OscillatorContext from '../../context/oscillatorContext/oscillatorContext';
 import styles from './Keyboard.module.scss';
 
-const Key = ({
-  index,
-  keyName,
-  windowWidth,
-  currentOctave,
-  keyNamesType,
-  // qwertyLabel,
-  // setQwertyLabel,
-}) => {
+const Key = ({ index, keyName, windowWidth, currentOctave, keyNamesType }) => {
   const oscillatorContext = useContext(OscillatorContext);
   const { setNotePitch, setNotePlaying, setNoteVolume } = oscillatorContext;
-
-  // const keyRef = createRef();
-
-  // const [keyRefPosition, setKeyRefPosition] = useState();
-
-  // useEffect(() => {
-  //   let keyRefPosition = keyRef.current.getBoundingClientRect().right;
-  //   setKeyRefPosition(keyRefPosition);
-  // }, [keyRef]);
 
   // Style the keys
   const keyIsAccidental = keyName.match(/#|b/); // Get the sharps and flats
@@ -36,42 +18,11 @@ const Key = ({
   // C# and F# also need a different style
   const keyIsFirstSharp = keyName.match(/C#|Db|F#|Gb/);
   const firstSharp = keyIsFirstSharp ? `${styles.firstSharp}` : ``;
-
   const keyIsCNatural = keyName.match(/C/);
-  const cNatural = keyIsCNatural ? `${styles.cNatural}` : ``;
   const keyIsCSharp = keyName.match(/C#|Db/);
   const cSharp = keyIsCSharp ? `${styles.cSharp}` : ``;
-  // const keyIsDSharp = keyName.match(/D#/);
-  // const dSharp = keyIsDSharp ? `${styles.dSharp}` : ``;
   const keyIsFSharp = keyName.match(/F#|Gb/);
   const fSharp = keyIsFSharp ? `${styles.fSharp}` : ``;
-  // const keyIsGSharp = keyName.match(/G#/);
-  // const gSharp = keyIsGSharp ? `${styles.gSharp}` : ``;
-  // const keyIsASharp = keyName.match(/A#/);
-  // const aSharp = keyIsASharp ? `${styles.aSharp}` : ``;
-
-  // TODO: can probably remove the currentOctave stuff, because it's being handled by Keyboard.js
-  // Center the current octave on the screen
-  let currentOctaveGRegEx = new RegExp(`G${currentOctave}`);
-  const keyIsCurrentOctaveG = keyName.match(currentOctaveGRegEx);
-  // console.log(keyIsCurrentOctaveG);
-
-  // const keyNamesCurrentOctave = keyNames.filter((keyName) => {
-  //   // Get only the notes with the current octave number
-  //   let currentOctaveRegEx = new RegExp(currentOctave);
-  //   return keyName.match(currentOctaveRegEx);
-  // });
-
-  // Hide the keys if they go off screen, so that partial keys aren't shown
-  // This is using the VisibilitySensor package, which includes a throttle on the resize event and stuff
-  // It would be nice if I only rendered the ones I need, but can't figure that out right now
-  const [isVisible, setIsVisible] = useState(true);
-  const onVisibilityChange = (isVisible) => {
-    // console.log('Element is now %s', isVisible ? 'visible' : 'hidden');
-    setIsVisible(isVisible);
-  };
-
-  let opacity = isVisible ? '1' : '0';
 
   // Set up the Qwerty key labels
   const [qwertyLabel, setQwertyLabel] = useState('');
@@ -161,9 +112,8 @@ const Key = ({
   return (
     // eslint-disable-next-line
     <li
+      // TODO: make this a button?
       className={`${styles.key} ${keyColor} ${noFlat} ${firstSharp} ${cSharp} ${fSharp}`}
-      style={{ opacity }}
-      // ref={keyRef}
       data-keyname={keyName}
       onMouseDown={handleNoteOn}
       onMouseUp={handleNoteOff}
@@ -171,30 +121,8 @@ const Key = ({
       onTouchStart={handleNoteOn}
       onTouchEnd={handleNoteOff}
     >
-      {/* TODO: make this a button? Remove the role="button" above.  */}
-      {/* {keyIsCNatural && !keyIsCSharp && <p>{keyName}</p>} */}
-      {/* {keyNamesType === 'none' ? '' : <p>{keyName}</p>} */}
       <p>{keyLabel}</p>
     </li>
-    //   <VisibilitySensor
-    //   key={index}
-    //   onChange={onVisibilityChange}
-    //   partialVisibility={true}
-    //   resizeThrottle="1000"
-    // >
-    //   {/* eslint-disable-next-line */}
-    //   <li
-    //     className={`${styles.key} ${keyColor} ${noFlat} ${firstSharp} ${cSharp} ${dSharp} ${fSharp} ${gSharp} ${aSharp}`}
-    //     style={{ opacity }}
-    //     // ref={keyRef}
-    //     data-keyname={keyName}
-    //     onMouseDown={handleNoteOn}
-    //     onMouseUp={handleNoteOff}
-    //   >
-    //     {/* TODO: make this a button? Remove the role="button" above. */}
-    //     {/* <p>{keyName}</p> */}
-    //   </li>
-    // </VisibilitySensor>
   );
 };
 
